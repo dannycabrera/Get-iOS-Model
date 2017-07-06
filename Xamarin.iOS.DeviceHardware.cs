169766bdb6505f0d998c745c833ba2ced8b699df
Jim Bennett <Jim.Bennett@eroad.com>
2017-07-06 15:56:39 +1200

    Creating a solution and adding nuget

--- Xamarin.iOS.DeviceHardware.cs
+++ Xamarin.iOS.DeviceHardware.cs
@@ -4,56 +4,59 @@ using ObjCRuntime;
 
 namespace Xamarin.iOS
 {
-    public static class DeviceHardware
+	public static class DeviceHardware
     {
-        public const string HardwareProperty = "hw.machine";
+        private const string HardwareProperty = "hw.machine";
 
         [DllImport(Constants.SystemLibrary)]
-        internal static extern int sysctlbyname([MarshalAs(UnmanagedType.LPStr)] string property, // name of the property
-                                                IntPtr output, // output
-                                                IntPtr oldLen, // IntPtr.Zero
-                                                IntPtr newp, // IntPtr.Zero
-                                                uint newlen // 0
-                                                );
-        public static string Version
+        private static extern int Sysctlbyname([MarshalAs(UnmanagedType.LPStr)] string property,
+                                                IntPtr output,
+                                                IntPtr oldLen,
+                                                IntPtr newp,
+                                                uint newlen);
+
+        private static readonly iOSHardware _hardwareMapper = new iOSHardware();
+        private static readonly Lazy<string> _version = new Lazy<string>(FindVersion);
+
+        private static string FindVersion()
         {
-            get
+            try
             {
-                try 
+                // get the length of the string that will be returned
+                var pLen = Marshal.AllocHGlobal(sizeof(int));
+                Sysctlbyname(HardwareProperty, IntPtr.Zero, pLen, IntPtr.Zero, 0);
+
+                var length = Marshal.ReadInt32(pLen);
+
+                // check to see if we got a length
+                if (length == 0)
                 {
-                    // get the length of the string that will be returned
-                    var pLen = Marshal.AllocHGlobal(sizeof(int));
-                    sysctlbyname(HardwareProperty, IntPtr.Zero, pLen, IntPtr.Zero, 0);
-                                        
-                    var length = Marshal.ReadInt32(pLen);
-                                        
-                    // check to see if we got a length
-                    if (length == 0)
-                    {
-                            Marshal.FreeHGlobal(pLen);
-                            return "Unknown";
-                    }
-                                        
-                    // get the hardware string
-                    var pStr = Marshal.AllocHGlobal(length);
-                    sysctlbyname(HardwareProperty, pStr, pLen, IntPtr.Zero, 0);
-                                        
-                    // convert the native string into a C# string
-                    var hardwareStr = Marshal.PtrToStringAnsi(pStr);
-                                        
-                    // cleanup
                     Marshal.FreeHGlobal(pLen);
-                    Marshal.FreeHGlobal(pStr);
-                                        
-                    return hardwareStr;
-                } 
-                catch (Exception ex) 
-                {
-                    Console.WriteLine("DeviceHardware.Version Ex: " + ex.Message);
+                    return "Unknown";
                 }
 
-                return "Unknown";
+                // get the hardware string
+                var pStr = Marshal.AllocHGlobal(length);
+                Sysctlbyname(HardwareProperty, pStr, pLen, IntPtr.Zero, 0);
+
+                // convert the native string into a C# string
+                var hardwareStr = Marshal.PtrToStringAnsi(pStr);
+
+                // cleanup
+                Marshal.FreeHGlobal(pLen);
+                Marshal.FreeHGlobal(pStr);
+
+                return hardwareStr;
             }
-        }
+            catch (Exception ex)
+            {
+                Console.WriteLine("DeviceHardware.Version Ex: " + ex.Message);
+            }
+
+            return "Unknown";
+		}
+
+        public static string Version => FindVersion();
+        public static string Model => _hardwareMapper.GetModel(Version);
     }
 }
