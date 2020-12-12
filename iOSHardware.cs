645004b654fcefa5d5faa869a6a8a7beb6dc4641
Danny Cabrera <1189224+dannycabrera@users.noreply.github.com>
2020-12-12 11:55:19 -0500

    iPad hardware strings updated
    iPad hardware strings updated

--- iOSHardware.cs
+++ iOSHardware.cs
@@ -121,121 +121,128 @@ namespace Xamarin.iOS
             {
                 switch (hardware)
                 {
-
+                    case "iPad13,2":
+                        return "iPad Air (4th generation) Wi-Fi + Cellular";
+                    case "iPad13,1":
+                        return "iPad Air (4th generation) Wi-Fi";
+                    case "iPad11,7":
+                        return "iPad (8th Generation) Wi-Fi + Cellular";
                     case "iPad11,6":
-                        return "iPad (8th Generation)";
-
-
-                    case "iPad1,1":
-                        return "iPad";
-                    case "iPad2,1":
-                        return "iPad 2 Wi-Fi";
-                    case "iPad2,2":
-                        return "iPad 2 GSM";
-                    case "iPad2,3":
-                        return "iPad 2 CDMA";
-                    case "iPad2,4":
-                        return "iPad 2 Wi-Fi";
-                    case "iPad3,1":
-                        return "iPad 3 Wi-Fi";
-                    case "iPad3,2":
-                        return "iPad 3 Wi-Fi + Cellular (VZ)";
-                    case "iPad3,3":
-                        return "iPad 3 Wi-Fi + Cellular";
-                    case "iPad3,4":
-                        return "iPad 4 Wi-Fi";
-                    case "iPad3,5":
-                        return "iPad 4 Wi-Fi + Cellular";
-                    case "iPad3,6":
-                        return "iPad 4 Wi-Fi + Cellular (MM)";
-                    case "iPad6,11":
-                        return "iPad 5 Wi-Fi";
-                    case "iPad6,12":
-                        return "iPad 5 Wi-Fi + Cellular";
-                    case "iPad7,5":
-                        return "iPad 6 Wi-Fi";
-                    case "iPad7,6":
-                        return "iPad 6 Wi-Fi + Cellular";
-                    case "iPad7,11":
-                        return "iPad 7 Wi-Fi";
-                    case "iPad7,12":
-                        return "iPad 7 Wi-Fi + Cellular";
-                    case "iPad4,1":
-                        return "iPad Air Wi-Fi";
-                    case "iPad4,2":
-                        return "iPad Air Wi-Fi + Cellular";
-                    case "iPad4,3":
-                        return "iPad Air Wi-Fi + Cellular (TD-LTE)";
-                    case "iPad5,3":
-                        return "iPad Air 2 Wi-Fi";
-                    case "iPad5,4":
-                        return "iPad Air 2 Wi-Fi + Cellular";
-                    case "iPad11,3":
-                        return "iPad Air 3 Wi-Fi";
+                        return "iPad (8th Generation) Wi-Fi";
                     case "iPad11,4":
-                        return "iPad Air 3 Wi-Fi + Cellular";
-                    case "iPad2,5":
-                        return "iPad mini Wi-Fi";
-                    case "iPad2,6":
-                        return "iPad mini Wi-Fi + Cellular";
-                    case "iPad2,7":
-                        return "iPad mini Wi-Fi + Cellular (MM)";
-                    case "iPad4,4":
-                        return "iPad mini 2 Wi-Fi";
-                    case "iPad4,5":
-                        return "iPad mini 2 Wi-Fi + Cellular";
-                    case "iPad4,6":
-                        return "iPad mini 2 Wi-Fi + Cellular (TD-LTE)";
-                    case "iPad4,7":
-                        return "iPad mini 3 Wi-Fi";
-                    case "iPad4,8":
-                        return "iPad mini 3 Wi-Fi + Cellular";
-                    case "iPad4,9":
-                        return "iPad mini 3 Wi-Fi + Cellular (TD-LTE)";
-                    case "iPad5,1":
-                        return "iPad mini 4";
-                    case "iPad5,2":
-                        return "iPad mini 4 Wi-Fi + Cellular";
-                    case "iPad11,1":
-                        return "iPad mini 5 Wi-Fi";
+                        return "iPad Air (3rd generation) Wi-Fi + Cellular";
+                    case "iPad11,3":
+                        return "iPad Air (3rd generation) Wi-Fi";
                     case "iPad11,2":
-                        return "iPad mini 5 Wi-Fi + Cellular";
-                    case "iPad6,3":
-                        return "iPad Pro (9.7-inch)";
-                    case "iPad6,4":
-                        return "iPad Pro (9.7-inch) Wi-Fi + Cellular";
-                    case "iPad7,3":
-                        return "iPad Pro (10.5-inch)";
-                    case "iPad7,4":
-                        return "iPad Pro (10.5-inch) Wi-Fi + Cellular";
-                    case "iPad6,7":
-                        return "iPad Pro 12.9-inch";
-                    case "iPad6,8":
-                        return "iPad Pro 12.9-inch Wi-Fi + Cellular";
-                    case "iPad7,1":
-                        return "iPad Pro 12.9-inch (2nd generation)";
-                    case "iPad7,2":
-                        return "iPad Pro 12.9-inch (2nd generation) Wi-Fi + Cellular";
-                    case "iPad8,5":
-                    case "iPad8,6":
-                        return "iPad Pro 12.9-inch (3rd generation)";
-                    case "iPad8,7":
-                    case "iPad8,8":
-                        return "iPad Pro 12.9-inch (3rd generation Wi-Fi + Cellular)";
-                    case "iPad8,11":
-                        return "iPad Pro 12.9-inch (4th generation)";
+                        return "iPad mini (5th generation) Wi-Fi + Cellular";
+                    case "iPad11,1":
+                        return "iPad mini (5th generation) Wi-Fi";
                     case "iPad8,12":
-                        return "iPad Pro 12.9-inch (4th generation Wi-Fi + Cellular)";
-                    case "iPad8,1":
-                    case "iPad8,2":
+                        return "iPad Pro (12.9-inch) (4th generation) Wi-Fi + Cellular";
+                    case "iPad8,11":
+                        return "iPad Pro (12.9-inch) (4th generation) Wi-Fi";
+                    case "iPad8,10":
+                        return "iPad Pro (11-inch) (2nd generation) Wi-Fi + Cellular";
+                    case "iPad8,9":
+                        return "iPad Pro (11-inch) (2nd generation) Wi-Fi";
+                    case "iPad8,8":
+                        return "iPad Pro 12.9-inch (3rd Generation)";
+                    case "iPad8,7":
+                        return "iPad Pro 12.9-inch (3rd generation) Wi-Fi + Cellular";
+                    case "iPad8,6":
+                        return "iPad Pro 12.9-inch (3rd Generation)";
+                    case "iPad8,5":
+                        return "iPad Pro 12.9-inch (3rd Generation)";
+                    case "iPad8,4":
                         return "iPad Pro 11-inch";
                     case "iPad8,3":
-                    case "iPad8,4":
                         return "iPad Pro 11-inch Wi-Fi + Cellular";
-                    case "iPad8,9":
-                        return "iPad Pro 11-inch (2nd generation)";
-                    case "iPad8,10":
-                        return "iPad Pro 11-inch (2nd generation) Wi-Fi + Cellular";
+                    case "iPad8,2":
+                        return "iPad Pro 11-inch";
+                    case "iPad8,1":
+                        return "iPad Pro 11-inch Wi-Fi";
+                    case "iPad7,12":
+                        return "iPad (7th generation) Wi-Fi + Cellular";
+                    case "iPad7,11":
+                        return "iPad (7th generation) Wi-Fi";
+                    case "iPad7,6":
+                        return "iPad (6th generation) Wi-Fi + Cellular";
+                    case "iPad7,5":
+                        return "iPad (6th generation) Wi-Fi";
+                    case "iPad7,4":
+                        return "iPad Pro (10.5-inch) Wi-Fi + Cellular";
+                    case "iPad7,3":
+                        return "iPad Pro (10.5-inch) Wi-Fi";
+                    case "iPad7,2":
+                        return "iPad Pro 12.9-inch (2nd generation) Wi-Fi + Cellular";
+                    case "iPad7,1":
+                        return "iPad Pro 12.9-inch (2nd generation) Wi-Fi";
+                    case "iPad6,12":
+                        return "iPad (5th generation) Wi-Fi + Cellular";
+                    case "iPad6,11":
+                        return "iPad (5th generation) Wi-Fi";
+                    case "iPad6,8":
+                        return "iPad Pro 12.9-inch Wi-Fi + Cellular";
+                    case "iPad6,7":
+                        return "iPad Pro 12.9-inch Wi-Fi";
+                    case "iPad6,4":
+                        return "iPad Pro (9.7-inch) Wi-Fi + Cellular";
+                    case "iPad6,3":
+                        return "iPad Pro (9.7-inch) Wi-Fi";
+                    case "iPad5,4":
+                        return "iPad Air 2 Wi-Fi + Cellular";
+                    case "iPad5,3":
+                        return "iPad Air 2 Wi-Fi";
+                    case "iPad5,2":
+                        return "iPad mini 4 Wi-Fi + Cellular";
+                    case "iPad5,1":
+                        return "iPad mini 4 Wi-Fi";
+                    case "iPad4,9":
+                        return "iPad mini 3 Wi-Fi + Cellular (TD-LTE)";
+                    case "iPad4,8":
+                        return "iPad mini 3 Wi-Fi + Cellular";
+                    case "iPad4,7":
+                        return "iPad mini 3 Wi-Fi";
+                    case "iPad4,6":
+                        return "iPad mini 2 Wi-Fi + Cellular (TD-LTE)";
+                    case "iPad4,5":
+                        return "iPad mini 2 Wi-Fi + Cellular";
+                    case "iPad4,4":
+                        return "iPad mini 2 Wi-Fi";
+                    case "iPad4,3":
+                        return "iPad Air Wi-Fi + Cellular (TD-LTE)";
+                    case "iPad4,2":
+                        return "iPad Air Wi-Fi + Cellular";
+                    case "iPad4,1":
+                        return "iPad Air Wi-Fi";
+                    case "iPad3,6":
+                        return "iPad (4th generation) Wi-Fi + Cellular (MM)";
+                    case "iPad3,5":
+                        return "iPad (4th generation) Wi-Fi + Cellular";
+                    case "iPad3,4":
+                        return "iPad (4th generation) Wi-Fi";
+                    case "iPad3,3":
+                        return "iPad 3 Wi-Fi + Cellular (CDMA)";
+                    case "iPad3,2":
+                        return "iPad 3 Wi-Fi + Cellular (GSM)";
+                    case "iPad3,1":
+                        return "iPad 3 Wi-Fi";
+                    case "iPad2,7":
+                        return "iPad mini Wi-Fi + Cellular (MM)";
+                    case "iPad2,6":
+                        return "iPad mini Wi-Fi + Cellular";
+                    case "iPad2,5":
+                        return "iPad mini Wi-Fi";
+                    case "iPad2,4":
+                        return "iPad 2 Wi-Fi";
+                    case "iPad2,3":
+                        return "iPad 2 CDMA";
+                    case "iPad2,2":
+                        return "iPad 2 GSM";
+                    case "iPad2,1":
+                        return "iPad 2 Wi-Fi";
+                    case "iPad1,1":
+                        return "iPad";
                 }
             }
 
